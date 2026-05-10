8f87676655a28955f531afb589ac276b9748426a
Suk Hyun Sung <sukhsung@umich.edu>
2026-05-10 00:06:58 -0400

    Info manager is incldued

--- src/renderer/js/splash/UI_InfoManager.js
+++ src/renderer/js/splash/UI_InfoManager.js
@@ -1,22 +1,26 @@
-import { import_template } from "../util/import_template.js";
 import { UI_SplashManager } from "./UI_SplashManager.js";
+import { instrumentTemplateUrl } from "../util/package_urls.js";
 
 export { UI_InfoManager };
 
 class UI_InfoManager extends UI_SplashManager {
-  constructor() {
+  constructor(url = "", copyright = "") {
     super(
-      "./templates/template_info.html",
+      instrumentTemplateUrl("template_info.html"),
       "info-overlay",
       "PB_info",
       "PB_info_close",
       true,
       false,
     );
+    this.url = url;
+    this.copyright = copyright;
   }
 
   async _create_page() {
     this.version = await window.api_app.get_version();
     document.getElementById("info_version").textContent = `v${this.version}`;
+    document.getElementById("info_url").textContent = this.url;
+    document.getElementById("info_copyright").textContent = this.copyright;
   }
 }
